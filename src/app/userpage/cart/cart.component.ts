da879ba5ba2606bebf7757be436893606ecd0d98
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-05-10 11:45:34 +0800

    [ADD] Add cart component

---
+++ src/app/userpage/cart/cart.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+
+  constructor(public router: Router) { }
+
+  ngOnInit(): void {
+  }
+
+  backToHome(){
+    this.router.navigate(["/userpage"])
+  }
+
+}
