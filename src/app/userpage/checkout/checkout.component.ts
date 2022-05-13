116da3b95939302be64c1c87e0d0394f3241cd07
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-05-13 18:34:42 +0800

    [ADD] add cart component

---
+++ src/app/userpage/checkout/checkout.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-checkout',
+  templateUrl: './checkout.component.html',
+  styleUrls: ['./checkout.component.css']
+})
+export class CheckoutComponent implements OnInit {
+  firstFormGroup: FormGroup;
+  secondFormGroup: FormGroup;
+
+  constructor(private _formBuilder: FormBuilder) {
+    this.firstFormGroup = this._formBuilder.group({
+      firstCtrl: ['', Validators.required]
+    });
+    this.secondFormGroup = this._formBuilder.group({
+      secondCtrl: ['', Validators.required]
+    });
+  }
+
+  ngOnInit() {
+   
+  }
+
+  openCheckout(){
+    
+  }
+
+}
