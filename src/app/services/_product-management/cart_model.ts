e43dac05d696b666b5d0711a8a4cf1a0b215053b
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-05-15 00:08:53 +0800

    [ADD] Add cart model

---
+++ src/app/services/_product-management/cart_model.ts
@@ -0,0 +1,9 @@
+export interface Cart{
+    id: string;
+    productid: string;
+    customerid: string;
+    specification: string;
+    price: string;
+    created: string;
+    isDeleted: boolean;
+}
