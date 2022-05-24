4233e74b4e074418ee2a82ff524be7d814e4de37
Jefferson Jadoc <70456980+ashwallbera@users.noreply.github.com>
2022-05-24 09:41:45 +0800

    [MOD] Checkout proceed to shipping validation

--- src/app/userpage/checkout/checkout.component.ts
+++ src/app/userpage/checkout/checkout.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { Router } from '@angular/router';
+import { ConfirmDialogComponent } from 'src/app/confirm-dialog/confirm-dialog.component';
 import { LoginModel } from 'src/app/services/_login/login_model';
 import { Cart } from 'src/app/services/_product-management/cart_model';
 import { ProductService } from 'src/app/services/_product-management/product.service';
@@ -16,7 +17,8 @@ export class CheckoutComponent implements OnInit {
   firstFormGroup: FormGroup;
   secondFormGroup: FormGroup;
   thirdFormGroup: FormGroup;
-  
+  error = false;
+  dialog: any;
 
   constructor(
     private _formBuilder: FormBuilder,
@@ -42,29 +44,46 @@ export class CheckoutComponent implements OnInit {
   openCheckout() {}
 
   getInfo() {
-    let user: LoginModel[] = JSON.parse(""+localStorage.getItem('dpuser'));
-    console.log(this.firstFormGroup.get('firstCtrl')?.value);
-    console.log(this.secondFormGroup.get('secondCtrl')?.value);
-    console.log(this.thirdFormGroup.get('thirdCtrl')?.value);
+    if(this.firstFormGroup.get('firstCtrl')?.value != "" || this.secondFormGroup.get('secondCtrl')?.value != "" || this.thirdFormGroup.get('thirdCtrl')?.value != ""){
 
-    // create shipping object
-    let v : Shipping;
-    v = {
-      id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-      customerid: user[0].id,
-      orderno: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
-      status: "Order Received by admin",
-      address: this.secondFormGroup.get('secondCtrl')?.value+"",
-      fullname: this.firstFormGroup.get('firstCtrl')?.value+"",
-      paymentMethod: this.thirdFormGroup.get('thirdCtrl')?.value+"",
-      isDeleted: false,
-      cart: this.cart
+      let user: LoginModel[] = JSON.parse(""+localStorage.getItem('dpuser'));
+      console.log(this.firstFormGroup.get('firstCtrl')?.value);
+      console.log(this.secondFormGroup.get('secondCtrl')?.value);
+      console.log(this.thirdFormGroup.get('thirdCtrl')?.value);
+  
+      // create shipping object
+      let v : Shipping;
+      v = {
+        id: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        customerid: user[0].id,
+        orderno: "3fa85f64-5717-4562-b3fc-2c963f66afa6",
+        status: "Order Received by admin",
+        address: this.secondFormGroup.get('secondCtrl')?.value+"",
+        fullname: this.firstFormGroup.get('firstCtrl')?.value+"",
+        paymentMethod: this.thirdFormGroup.get('thirdCtrl')?.value+"",
+        isDeleted: false,
+        cart: this.cart
+      }
+  
+      this.product_service.addToShipping(v);
+      this.router.navigate(["/shipping"]);
+      
     }
-
-    this.product_service.addToShipping(v);
-    this.router.navigate(["/shipping"]);
+    
+    else{
+      const dialogRef = this.dialog.open(ConfirmDialogComponent,{
+        data: "Checkout failed!"
+      });
+    }
+  
     // call the shipping api
     
   
+  }
+  getErrorMessage() {
+    if (this.error ) {
+      return 'USERNAME OR PASSWORD INCORRECT';
+    }
+    return '';
   }
 }
